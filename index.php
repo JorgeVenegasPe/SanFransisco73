4e5194b407326c49bc61e32bfe228489db052c71
JorgeVenegasPe <mariano.venegas.francia@gmail.com>
2023-10-15 18:14:26 -0500

    Master

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="resources/css/index.css">
+</head>
+<body>
+    <?php
+    require 'views/header.php';
+    ?>
+    <section class="home flex-center" id="home">
+        <div class="home-container">
+            <div class="info">
+                <p>Una herramienta de IA diseñada para saber el tipo de corte según su cráneo</p>
+                <h2>Recomendación de cortes de cabello Escaneo craneal usando IA</h2>
+                <a class="button-signup" href="#contacto">Create una cuenta</a>
+                <p>7 Días Prueba gratuita | Uso para todos los clientes</p>
+            </div>
+        </div>
+        <video ></video>
+    </section>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="nav-bar">
+        <a href="" class="logo"><img src="resources/images/LogoSF3.png" alt=""></a>
+        <div class="navigation">
+            <div class="nav-items">
+                <a href="#home">Home</a>
+                <a href="#Catalogo">Catalogo</a>
+                <a href="#Servicios">Servicios</a>
+                <a href="#servicios">IA</a>
+            </div>
+        </div> 
+        <div class="navigation">
+            <div class="nav-items">
+                <a href="#Login">Iniciar Sesión</a>
+                <a class="button-signup" href="#contacto">Registrar</a>
+            </div>
+        </div>  
+    </div>
+</header>
