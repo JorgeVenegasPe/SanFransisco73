69be06b35135b173b23de587ae920d97ce5a9018
JorgeVenegasPe <130241253+JorgeVenegasPe@users.noreply.github.com>
2023-11-07 11:20:42 -0500

    Add files via upload

---
+++ SanFransisco73-main/views/footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+            <div class="footer__container">
+                <div class="footer__top">
+                    <div class="company__info">
+                        <h2  class="company__logo">San Fransisco 73</h2>
+                        <p class="company__description">
+                            ¡Gracias por visitar San Fransisco 73! Esperamos que hayas disfrutado explorando nuestras opciones de cortes de cabello y servicios de barbería. 
+                            Nos apasiona ofrecer a nuestros clientes las mejores experiencias de cuidado y estilo capilar. Siempre nos esforzamos por proporcionar un servicio 
+                            excepcional y garantizar que cada visita sea única. Esperamos que hayas encontrado el corte o servicio que estabas buscando y que estés emocionado 
+                            por descubrir lo que [Nombre de la Barbería] tiene para ofrecer. Gracias de nuevo por visitarnos. Si tienes alguna pregunta o necesitas ayuda para
+                            planificar tu próximo corte o estilo, no dudes en contactarnos. ¡Te deseamos una experiencia capilar excepcional llena de estilo y confianza!"
+                        </p>
+                        <ul class="footer__list">
+                            <li class="footer__list-item">
+                                <a href="https://www.facebook.com/people/Jorge-Venegas/pfbid0aGzXozbxooxMzNbvQoVjA9HKmdK9CQ7xYCHjrqHrowDYXCgtrV7iCRVj1Nm3igu8l/" target="_blank" class="footer__list-link">
+                                    <i class="ri-facebook-circle-line"></i>
+                                </a>
+                            </li>
+                            <li class="footer__list-item">
+                                <a href="https://www.instagram.com/jorgevenegs/" target="_blank" class="footer__list-link">
+                                    <i class="ri-instagram-line"></i>
+                                </a>
+                            </li>
+                            <li class="footer__list-item">
+                                <a href="https://www.tiktok.com/@jorge.pe_" target="_blank" class="footer__list-link">
+                                <i class="fa-brands fa-tiktok"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <span class="copyright">&copy;2023 Autor: Jorge Venegas.</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
---
+++ SanFransisco73-main/views/header.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="nav-bar">
+        <a href="#home" class="logo"><img src="resources/images/LogoSF3.png" alt=""></a>
+        <div class="navigation">
+            <div class="nav-items">
+                <a href="#nosotros">Nosotros</a>
+                <a href="#services">Servicios</a>
+                <a href="#reserva">Reservar cita</a>
+                <a href="Int-Art.php">IA</a>
+            </div>
+        </div> 
+        <div class="navigation">
+            <div class="nav-items">
+                <!-- <a href="../SanFransisco73-main/Login_Registro.php#login-form">Iniciar Sesión</a>
+                <a class="button-signup" href="../SanFransisco73-main/Login_Registro.php#signup-form">Registrar</a> -->
+<!-- 
+                <a href="../SanFransisco73-main/Login_Registro.php">Iniciar Sesión</a>
+            <a class="button-signup register-link" href="../SanFransisco73-main/Login_Registro.php">Registrar</a> -->
+
+            <a href="../SanFransisco73-main/Login_Registro.php">Iniciar Sesión</a>
+                <a class="button-signup" href="../SanFransisco73-main/Login_Registro.php">Registrar</a>
+            </div>
+        </div>  
+    </div>
+</header>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="./resources/js/app.js"></script>
+
